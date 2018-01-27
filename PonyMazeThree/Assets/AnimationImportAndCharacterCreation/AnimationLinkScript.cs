dbf80d2dd2aa1c7bec91f764d59691a6a1ecac1f
Yatkal <yatkalsecond@gmail.com>
2018-01-27 11:58:37 +0000

    Old Project commit

---
+++ PonyMazeThree/Assets/AnimationImportAndCharacterCreation/AnimationLinkScript.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+public class AnimationLinkScript : MonoBehaviour {
+
+    public GameObject headAnimation;
+    public GameObject bodyAnimation;
+    GameObject bodyModel;
+    GameObject headModel;
+    GameObject finishedModel;
+    List<GameObject> listOfChildrenOfBodyModel;
+    List<GameObject> listOfChildrenOfHeadModel;
+    List<GameObject> listOfChildrenOfBodyAnimation;
+    List<GameObject> listOfChildrenOfHeadAnimation;
+
+    // Use this for initialization
+    void Start () {
+        listOfChildrenOfBodyModel = new List<GameObject>();
+        listOfChildrenOfHeadModel = new List<GameObject>();
+        listOfChildrenOfBodyAnimation = new List<GameObject>();
+        listOfChildrenOfHeadAnimation = new List<GameObject>();
+        finishedModel = new GameObject("Model");
+        bodyAnimation = GameObject.Find("UnparentedBodyWalk").transform.GetChild(0).gameObject;
+        headAnimation = GameObject.Find("UnparentedHeadWalk").transform.GetChild(0).gameObject;
+        bodyModel = transform.FindChild("SoriaModel(Clone)").gameObject;
+        headModel = transform.FindChild("Octavia(Clone)").gameObject;
+
+        GetChildRecursiveForBodyModel(bodyModel);
+        GetChildRecursiveForHeadModel(headModel);
+        GetChildRecursiveForBodyAnimation(bodyAnimation);
+        GetChildRecursiveForHeadAnimation(headAnimation);
+
+        MatchAnimationToModel();
+
+        StartCoroutine(FinishModel());
+
+        //Debug.Log("NUMBER OF CHILDREN");
+        //Debug.Log(listOfChildrenOfAnimation.Count);
+	}
+
+    void MatchAnimationToModel()
+    {
+        for(int iter = 0; iter < listOfChildrenOfBodyAnimation.Count; iter++)
+        {
+            for(int iter2 = 0; iter2 < listOfChildrenOfBodyModel.Count; iter2++)
+            {
+                if(listOfChildrenOfBodyAnimation[iter].name == listOfChildrenOfBodyModel[iter2].name)
+                {
+                    //listOfChildrenOfModel[iter2].transform.DetachChildren();
+                    listOfChildrenOfBodyModel[iter2].transform.parent = listOfChildrenOfBodyAnimation[iter].transform;
+                    listOfChildrenOfBodyModel[iter2].AddComponent<AlwaysAtParentScript>();
+                    //listOfChildrenOfModel[iter2].transform.localRotation = Quaternion.identity;
+                    //listOfChildrenOfModel[iter2].transform.localPosition = Vector3.zero;
+                    //listOfChildrenOfModel[iter2].transform.rotation = Quaternion.identity;
+                    //listOfChildrenOfModel[iter2].transform.position = Vector3.zero;
+                    //listOfChildrenOfModel[iter2].transform.localScale = listOfChildrenOfAnimation[iter2].transform.localScale;
+                    iter2 = listOfChildrenOfBodyModel.Count;
+                }
+            }
+        }
+
+        for (int iter = 0; iter < listOfChildrenOfHeadAnimation.Count; iter++)
+        {
+            for (int iter2 = 0; iter2 < listOfChildrenOfHeadModel.Count; iter2++)
+            {
+                if (listOfChildrenOfHeadAnimation[iter].name == listOfChildrenOfHeadModel[iter2].name)
+                {
+                    //listOfChildrenOfModel[iter2].transform.DetachChildren();
+                    listOfChildrenOfHeadModel[iter2].transform.parent = listOfChildrenOfHeadAnimation[iter].transform;
+                    listOfChildrenOfHeadModel[iter2].AddComponent<AlwaysAtParentScript>();
+                    //listOfChildrenOfModel[iter2].transform.localRotation = Quaternion.identity;
+                    //listOfChildrenOfModel[iter2].transform.localPosition = Vector3.zero;
+                    //listOfChildrenOfModel[iter2].transform.rotation = Quaternion.identity;
+                    //listOfChildrenOfModel[iter2].transform.position = Vector3.zero;
+                    //listOfChildrenOfModel[iter2].transform.localScale = listOfChildrenOfAnimation[iter2].transform.localScale;
+                    iter2 = listOfChildrenOfHeadModel.Count;
+                }
+            }
+        }
+    }
+
+    IEnumerator FinishModel()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        headAnimation.transform.root.transform.parent = finishedModel.transform;
+        bodyAnimation.transform.root.transform.parent = finishedModel.transform;
+
+        headAnimation.transform.parent.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        bodyAnimation.transform.parent.transform.position = Vector3.zero;
+        headAnimation.transform.parent.transform.position = Vector3.zero;
+        headAnimation.transform.parent.transform.Rotate(0.0f, 180.0f, 180.0f);
+        headAnimation.transform.parent.transform.position = new Vector3(-0.52f, -1.4f, -17.76f);
+    }
+
+    private void GetChildRecursiveForBodyModel(GameObject obj)
+    {
+        if (null == obj)
+        {
+            return;
+        }
+
+        for (int iter = 0; iter < obj.transform.childCount; iter++)
+        {
+            if (null == obj.transform.GetChild(iter).gameObject)
+            {
+                continue;
+            }
+            //child.gameobject contains the current child you can do whatever you want like add it to an array
+            listOfChildrenOfBodyModel.Add(obj.transform.GetChild(iter).gameObject);
+            GetChildRecursiveForBodyModel(obj.transform.GetChild(iter).gameObject);
+        }
+    }
+
+    private void GetChildRecursiveForHeadModel(GameObject obj)
+    {
+        if (null == obj)
+        {
+            return;
+        }
+
+        for (int iter = 0; iter < obj.transform.childCount; iter++)
+        {
+            if (null == obj.transform.GetChild(iter).gameObject)
+            {
+                continue;
+            }
+            //child.gameobject contains the current child you can do whatever you want like add it to an array
+            listOfChildrenOfHeadModel.Add(obj.transform.GetChild(iter).gameObject);
+            GetChildRecursiveForHeadModel(obj.transform.GetChild(iter).gameObject);
+        }
+    }
+
+    private void GetChildRecursiveForBodyAnimation(GameObject obj)
+    {
+        if (null == obj)
+        {
+            return;
+        }
+
+        for (int iter = 0; iter < obj.transform.childCount; iter++)
+        {
+            if (null == obj.transform.GetChild(iter).gameObject)
+            {
+                continue;
+            }
+            //child.gameobject contains the current child you can do whatever you want like add it to an array
+            listOfChildrenOfBodyAnimation.Add(obj.transform.GetChild(iter).gameObject);
+            GetChildRecursiveForBodyAnimation(obj.transform.GetChild(iter).gameObject);
+        }
+    }
+
+    private void GetChildRecursiveForHeadAnimation(GameObject obj)
+    {
+        if (null == obj)
+        {
+            return;
+        }
+
+        for (int iter = 0; iter < obj.transform.childCount; iter++)
+        {
+            if (null == obj.transform.GetChild(iter).gameObject)
+            {
+                continue;
+            }
+            //child.gameobject contains the current child you can do whatever you want like add it to an array
+            listOfChildrenOfHeadAnimation.Add(obj.transform.GetChild(iter).gameObject);
+            GetChildRecursiveForHeadAnimation(obj.transform.GetChild(iter).gameObject);
+        }
+    }
+}
---
+++ PonyMazeThree/Assets/AnimationImportAndCharacterCreation/CreateBaseCharacterScript.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateBaseCharacterScript : MonoBehaviour {
+
+    public GameObject character;
+    public GameObject head;
+
+	// Use this for initialization
+	public void CreateCharacter (string characterName) {
+
+        string modelString = "Models/";
+
+        modelString += characterName;
+
+        GameObject temp = Instantiate((GameObject)Resources.Load("Models/SoriaModel"));
+        character = temp;
+        character.transform.parent = gameObject.transform;
+        GameObject temp2 = Instantiate((GameObject)Resources.Load(modelString));
+        head = temp2;
+        //head.transform.parent.position = new Vector3(-0.1f, -1.4f, -17.03f);
+        head.transform.parent = gameObject.transform;
+        ApplyBaseMaterials(characterName);
+
+        //gameObject.AddComponent<AnimationLinkScript>();
+
+	}
+	
+    public void RefreshCharacter(string characterName)
+    {
+        Destroy(head);
+        Destroy(character);
+        CreateCharacter(characterName);
+    } 
+
+    void ApplyBaseMaterials(string characterName)
+    {
+        Material[] materials;
+
+        string materialsNameB = "Models/Materials/B_";
+        string materialsNameP = "Models/Materials/P_";
+        string materialsNameA = "Models/Materials/A_";
+        string materialsNameH = "Models/Materials/H_";
+        string materialsNameEL = "Models/Materials/EL_";
+        string materialsNameER = "Models/Materials/ER_";
+        string materialsNameHA = "Models/Materials/HA_";
+        string materialsNameTA = "Models/Materials/TA_";
+
+        materialsNameB += characterName;
+        materialsNameP += characterName;
+        materialsNameA += characterName;
+        materialsNameH += characterName;
+        materialsNameEL += characterName;
+        materialsNameER += characterName;
+        materialsNameHA += characterName;
+        materialsNameTA += characterName;
+
+        materials = character.transform.FindChild("Reference").GetComponent<Renderer>().materials;
+
+        for(int iter = 0; materials.Length > iter; iter++)
+        {
+            if(materials[iter].name == "body (Instance)")
+            {
+                materials[iter].mainTexture = (Texture)Resources.Load(materialsNameB);
+                materials[iter].color = Color.white;
+            }
+            else if (materials[iter].name == "Privates (Instance)")
+            {
+                materials[iter].mainTexture = (Texture)Resources.Load(materialsNameP);
+                materials[iter].color = Color.white;
+            }
+            else if (materials[iter].name == "Arms (Instance)")
+            {
+                materials[iter].mainTexture = (Texture)Resources.Load(materialsNameA);
+                materials[iter].color = Color.white;
+            }
+        }
+
+        Material[] headMaterials;
+        Material[] hairMaterial;
+        Material[] tailMaterial;
+        headMaterials = head.transform.FindChild("Reference").GetComponent<Renderer>().materials;
+        hairMaterial = head.transform.FindChild("Hair").GetComponent<Renderer>().materials;
+        tailMaterial = head.transform.FindChild("Tail").GetComponent<Renderer>().materials;
+
+        for(int iter = 0; headMaterials.Length > iter; iter++)
+        {
+            if (headMaterials[iter].name == "body (Instance)")
+            {
+                headMaterials[iter].mainTexture = (Texture)Resources.Load(materialsNameH);
+                headMaterials[iter].color = Color.white;
+            }
+            else if (headMaterials[iter].name == "tongue (Instance)")
+            {
+                headMaterials[iter].mainTexture = (Texture)Resources.Load("Models/Materials/Generic_Tongue");
+                headMaterials[iter].color = Color.white;
+            }
+            else if (headMaterials[iter].name == "teeth (Instance)")
+            {
+                headMaterials[iter].mainTexture = (Texture)Resources.Load("Models/Materials/Generic_Teeth");
+                headMaterials[iter].color = Color.white;
+            }
+            else if (headMaterials[iter].name == "eyelashes (Instance)")
+            {
+                headMaterials[iter].mainTexture = (Texture)Resources.Load("Models/Materials/Generic_Eyelashes");
+                headMaterials[iter].color = Color.white;
+            }
+            else if (headMaterials[iter].name == "eyeball_l (Instance)")
+            {
+                headMaterials[iter].mainTexture = (Texture)Resources.Load(materialsNameEL);
+                headMaterials[iter].mainTextureOffset = new Vector2(0.8f, -0.2f);
+                headMaterials[iter].color = Color.white;
+            }
+            else if (headMaterials[iter].name == "eyeball_r (Instance)")
+            {
+                headMaterials[iter].mainTexture = (Texture)Resources.Load(materialsNameER);
+                headMaterials[iter].mainTextureOffset = new Vector2(0.2f, -0.2f);
+                headMaterials[iter].color = Color.white;
+            }
+
+        }
+
+        for(int iter = 0; hairMaterial.Length > iter; iter++)
+        {
+            if (hairMaterial[iter].name == "hair (Instance)")
+            {
+                hairMaterial[iter].mainTexture = (Texture)Resources.Load(materialsNameHA);
+                hairMaterial[iter].color = Color.white;
+            }
+        }
+        for (int iter = 0; tailMaterial.Length > iter; iter++)
+        {
+            if (tailMaterial[iter].name == "tail (Instance)")
+            {
+                tailMaterial[iter].mainTexture = (Texture)Resources.Load(materialsNameTA);
+                tailMaterial[iter].color = Color.white;
+            }
+        }
+    }
+}
